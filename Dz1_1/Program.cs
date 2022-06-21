b84ef83b8c5bde03255e2595e1056eedd0d17fee
Алексей <ics111@yandex.ru>
2022-06-21 21:21:22 +0500

    add Dz_1

--- Dz1_1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿int a = 5; int b = 69; int c = 1;
-int max = a;
-if ( a > max) max = a;
-if ( b > max) max = b;
-if ( c > max) max = c;
-Console.Write ("Максимальное число = ");
-Console.Write (max);
